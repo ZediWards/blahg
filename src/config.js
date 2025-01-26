676d2faf7ac8a8e928274989179c0b0315d2bc06
Zediwards <WebDefault@pm.me>
2025-01-26 00:36:00 -0500

    changed description

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Zediwards Blog";
-export const SITE_DESCRIPTION = "Learnings, thoughts, and notes";
+export const SITE_DESCRIPTION = "Learning about software development and having fun along the way.";
 export const RSS_URL = "https://zediwards.com/rss.xml"
+
